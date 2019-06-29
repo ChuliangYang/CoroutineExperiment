95180927a95d185d1a80040be9d873e6d0c8d04b
ChuliangYang <mrcarlosyang@gmail.com>
2019-06-28 22:50:26 -0400

    experiment

--- app/src/main/java/com/demo/cl/coroutineExperiment/Experiment.kt
+++ app/src/main/java/com/demo/cl/coroutineExperiment/Experiment.kt
@@ -1,9 +1,10 @@
 package com.demo.cl.coroutineExperiment
 
+import android.content.ClipData.Item
 import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.channels.*
+import java.math.BigInteger
 import java.util.concurrent.Executors
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.ContinuationInterceptor
@@ -23,26 +24,55 @@ suspend fun main() {
 /*
  父协程职责:1. 派生职责:所有子协程扩展自父协程  2.管理职责: 父协程取消时会去(软)取消所有子协程; 父协程要等待所有子协程执行完毕才算执行完毕
 */
-    val job = GlobalScope.launch {
-           val sendChannel= actor<Int> {
-                var count=0
-                for(i in channel){
-                    count+=i
-                    println("${Thread.currentThread()}---${count}")
-                }
-            }
-        repeat(5000) {
-            launch(Dispatchers.Default){
-//                mutex.withLock {
-                sendChannel.send(1)
+//    val job = GlobalScope.launch {
+//           val sendChannel= actor<Int> {
+//                var count=0
+//                for(i in channel){
+//                    count+=i
+//                    println("${Thread.currentThread()}---${count}")
 //                }
-            }
-        }
-        sendChannel.close()
+//            }
+//        repeat(5000) {
+//            launch(Dispatchers.Default){
+////                mutex.withLock {
+//                sendChannel.send(1)
+////                }
+//            }
+//        }
+//        sendChannel.close()
+//    }
+//    job.join()
+
+
+//    GlobalScope.launch(MyContinuationInterceptor()){
+////        repeat(10){
+////            Thread.sleep(1000)
+////            println("${Thread.currentThread()}")
+////        }
+//        launch {
+//
+//        }
+//        timeConsumingTask()
+//
+//    }.join()
+
+    withContext(Dispatchers.Default){
+        //构造子协程,模拟耗时任务
+        println("${Thread.currentThread()}-coroutineScope")
     }
-    job.join()
+    coroutineScope{
+        println("${Thread.currentThread()}-coroutineScope")
 
-
+    }
+    GlobalScope.launch(MyContinuationInterceptor()){
+        log.d("这里会被调度到")
+        delay(1000)//这里由suspend的函数自调度
+        log.d("这里也会被调度到")
+        withContext(Dispatchers.Default){//这里由协程内部自行调度
+            println("这里受协程内自调度")
+        }
+        log.d("这里也会被调度到")
+    }
 
 }
 
@@ -50,7 +80,6 @@ class MyContinuationInterceptor : ContinuationInterceptor {
     override val key = ContinuationInterceptor
 
     override fun <T> interceptContinuation(continuation: Continuation<T>): Continuation<T> {
-        println("${Thread.currentThread()}-interceptContinuation")
         return MyContinuation(continuation)
     }
 
@@ -60,30 +89,45 @@ class MyContinuation<T>(val continuation: Continuation<T>) : Continuation<T> {
     private val executor = Executors.newSingleThreadExecutor { Thread(it, "myThread") }
     override val context: CoroutineContext = continuation.context
     override fun resumeWith(result: Result<T>) {
-        println("${Thread.currentThread()}-MyContinuation-resumeWith-1")
         executor.submit {
-            println("${Thread.currentThread()}-MyContinuation-executor-1")
             continuation.resumeWith(result)
-            println("${Thread.currentThread()}-MyContinuation-executor-2")
         }
-        println("${Thread.currentThread()}-MyContinuation-resumeWith-2")
     }
 
 }
 
 suspend fun timeConsumingTask() {
-//    println("${Thread.currentThread()}-startNormalTask")
-//    Thread.sleep(1000)
-//    println("${Thread.currentThread()}-endNormalTask")
-//
 //    withContext(Dispatchers.Default){
-//        println("${Thread.currentThread()}-startSuspendTask")
+//        //构造子协程,模拟耗时任务
 //        Thread.sleep(1000)
-//        println("${Thread.currentThread()}-endSuspendTask")
 //    }
-//    println("${Thread.currentThread()}-afterCoroutine")
-    timeConsumingTask1()
+    //调用suspend函数
+    coroutineScope(){
 
+    }
+    println("${Thread.currentThread()}-1")
+    withContext(Dispatchers.Default){
+        //构造子协程,模拟耗时任务
+        println("${Thread.currentThread()}-coroutineScope")
+    }
+    println("${Thread.currentThread()}-2")
+    withContext(Dispatchers.Default){
+        //构造子协程,模拟耗时任务
+        println("${Thread.currentThread()}-coroutineScope")
+    }
+    println("${Thread.currentThread()}-3")
+    withContext(Dispatchers.Default){
+        //构造子协程,模拟耗时任务
+        println("${Thread.currentThread()}-coroutineScope")
+    }
+    println("${Thread.currentThread()}-4")
 }
 
 suspend fun timeConsumingTask1() {}
+
+fun sequential(){
+    invoke1()
+    invoke2()
+    invoke3()
+}
+

6cf20fbf6fd6a3079eb2f4782d3ebdc787403d2b
ChuliangYang <mrcarlosyang@gmail.com>
2019-07-01 21:30:59 -0400

    experiment

--- app/src/main/java/com/demo/cl/coroutineExperiment/Experiment.kt
+++ app/src/main/java/com/demo/cl/coroutineExperiment/Experiment.kt
@@ -1,80 +1,104 @@
 package com.demo.cl.coroutineExperiment
 
-import android.content.ClipData.Item
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.channels.*
-import java.math.BigInteger
+import kotlinx.coroutines.sync.withLock
 import java.util.concurrent.Executors
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
+import kotlin.system.measureTimeMillis
 
 
 suspend fun main() {
     var count = 0
-    val mutex=Mutex()
+    val mutex = Mutex()
 
-//    runBlocking {
-//        Thread.sleep(4000)
-//        println("${Thread.currentThread()}-MyContinuation-resumeWith-1")
-//    }
-//    println("${Thread.currentThread()}-MyContinuation-resumeWith")
-
-/*
- 父协程职责:1. 派生职责:所有子协程扩展自父协程  2.管理职责: 父协程取消时会去(软)取消所有子协程; 父协程要等待所有子协程执行完毕才算执行完毕
-*/
-//    val job = GlobalScope.launch {
-//           val sendChannel= actor<Int> {
-//                var count=0
-//                for(i in channel){
-//                    count+=i
-//                    println("${Thread.currentThread()}---${count}")
-//                }
-//            }
-//        repeat(5000) {
-//            launch(Dispatchers.Default){
-////                mutex.withLock {
-//                sendChannel.send(1)
-////                }
-//            }
-//        }
-//        sendChannel.close()
-//    }
-//    job.join()
-
-
-//    GlobalScope.launch(MyContinuationInterceptor()){
-////        repeat(10){
-////            Thread.sleep(1000)
-////            println("${Thread.currentThread()}")
-////        }
-//        launch {
-//
-//        }
-//        timeConsumingTask()
-//
-//    }.join()
-
-    withContext(Dispatchers.Default){
-        //构造子协程,模拟耗时任务
-        println("${Thread.currentThread()}-coroutineScope")
-    }
-    coroutineScope{
-        println("${Thread.currentThread()}-coroutineScope")
-
-    }
-    GlobalScope.launch(MyContinuationInterceptor()){
-        log.d("这里会被调度到")
-        delay(1000)//这里由suspend的函数自调度
-        log.d("这里也会被调度到")
-        withContext(Dispatchers.Default){//这里由协程内部自行调度
-            println("这里受协程内自调度")
+    val time = measureTimeMillis {
+        val job = GlobalScope.launch {
+            val sendChannel = actor<Int> {
+                for (i in channel) {
+                    count += i
+                }
+            }
+            //使用父协程包裹10000个子协程,这样可以等待所有子协程完工后(即10000个数据都产生之后)才让channel关掉
+            //如果不用父协程包裹而直接在repeat之外关channel的话,就会导致10000个数据来不及产生完就被close掉
+            val plusJob = launch {
+                repeat(100000) {
+                    launch(Dispatchers.Default) {
+                        sendChannel.send(1)
+                    }
+                }
+            }
+            plusJob.join()//等待10000个数据产生完毕
+            sendChannel.close()
         }
-        log.d("这里也会被调度到")
+        job.join()
     }
+    println("$count/$time")
+
+    count = 0
+
+    val time2 = measureTimeMillis {
+        val job = GlobalScope.launch {
+            repeat(100000) {
+                launch(Dispatchers.Default) {
+                    mutex.withLock {
+                        count++
+                    }
+                }
+            }
+        }
+        job.join()
+    }
+    println("$count/$time2")
+
+    highOrderFunction(5, 4) { a: Int, b: Int -> a + b }
+    highOrderFunctionR(5, 4) {
+        //这里的接收器(context)是Int,也就是相当于这块代码写在Int类中
+        //this指向接收器,这里是5,it指向唯一的入参,这里是4
+        this + it
+    }
+
+    val list: List<*> = listOf<Int>(1, 2, 3, 4)
 
 }
+
+
+//    val handler = CoroutineExceptionHandler { _, exception ->
+//        println("Caught $exception")
+//    }
+//    val job = GlobalScope.launch(handler) {
+//        launch(CoroutineExceptionHandler { coroutineContext, throwable ->
+//            println("Caught1 $throwable")
+//        }) { // the first child
+//           kotlinx.coroutines.delay(500)
+//            throw ArrayIndexOutOfBoundsException()
+//        }
+//        launch(CoroutineExceptionHandler { coroutineContext, throwable ->
+//            println("Caught2 $throwable")
+//        }) { // the second child
+//            try {
+//                kotlinx.coroutines.delay(5000)
+//                println("1")
+//            }catch (e: Exception){
+//                println("Caught2 $e")
+//            }
+//
+//        }
+//
+//        supervisorScope{
+//
+//            launch(CoroutineExceptionHandler { coroutineContext, throwable ->
+//                println("Caught2 $throwable")
+//            }) { // the first child
+//                kotlinx.coroutines.delay(1000)
+//                throw ArrayIndexOutOfBoundsException()
+//            }
+//        }
+//    }.join()
+
 
 class MyContinuationInterceptor : ContinuationInterceptor {
     override val key = ContinuationInterceptor
@@ -102,21 +126,21 @@ suspend fun timeConsumingTask() {
 //        Thread.sleep(1000)
 //    }
     //调用suspend函数
-    coroutineScope(){
+    coroutineScope() {
 
     }
     println("${Thread.currentThread()}-1")
-    withContext(Dispatchers.Default){
+    withContext(Dispatchers.Default) {
         //构造子协程,模拟耗时任务
         println("${Thread.currentThread()}-coroutineScope")
     }
     println("${Thread.currentThread()}-2")
-    withContext(Dispatchers.Default){
+    withContext(Dispatchers.Default) {
         //构造子协程,模拟耗时任务
         println("${Thread.currentThread()}-coroutineScope")
     }
     println("${Thread.currentThread()}-3")
-    withContext(Dispatchers.Default){
+    withContext(Dispatchers.Default) {
         //构造子协程,模拟耗时任务
         println("${Thread.currentThread()}-coroutineScope")
     }
@@ -125,9 +149,32 @@ suspend fun timeConsumingTask() {
 
 suspend fun timeConsumingTask1() {}
 
-fun sequential(){
-    invoke1()
-    invoke2()
-    invoke3()
+fun highOrderFunction(first: Int, second: Int, transform: (Int, Int) -> Int): Int {
+    return transform(first, second)
 }
+
+fun highOrderFunctionR(first: Int, second: Int, transform: Int.(Int) -> Int): Int {
+//    transform(first, second)//可以这么写
+    return first.transform(second)//扩展函数写法
+}
+
+fun operation(opCode: Int): ((Int, Int) -> Int)? {
+//    returnNothing()
+//    returnUnit()
+    when (opCode) {
+        0 -> return { a, b -> a + b }
+        1 -> return { a: Int, b: Int -> a - b }
+        2 -> return { a, b -> a * b }
+        3 -> return { a, b -> a / b }
+        else -> return null
+    }
+
+    println("${Thread.currentThread()}-4")
+}
+
+inline fun <reified T> testReified() {
+    val i: Boolean = "Test" is T
+}
+
+
 
